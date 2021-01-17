7522b8f220d8dd4e4cb6f0b183b5072575f0768f
Ilia Burakov <burakov.ilia@gmail.com>
2021-01-18 00:40:56 +0800

    init

---
+++ FixUtf8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+
+string line;
+
+using var srcFile = new StreamReader(args[0]);
+using var destFile = new StreamWriter(args[1], false, Encoding.UTF8);
+try
+{
+    while ((line = srcFile.ReadLine()) != null)
+    {
+        destFile.WriteLine(line);
+    }
+}
+finally
+{
+    destFile.Close();
+    srcFile.Close();
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FixUtf8
 remove invalid byte sequences (UTF-8)
+
+```powershell
+Get-ChildItem "C:\files" | Foreach-Object { &"FixUtf8.exe" $_.FullName ($_.FullName + ".conv") }
+```
